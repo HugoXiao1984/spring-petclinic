78b29200b262fd84777cb63f5ad8dd611b133f6c
HugoXiao1984 <54834326+HugoXiao1984@users.noreply.github.com>
2021-04-07 13:28:46 +0800

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -23,7 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * PetClinic Spring Boot Application.
  *
  * @author Dave Syer
- *
+ * @author hugo xiao
  */
 @SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication {
